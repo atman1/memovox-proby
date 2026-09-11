35a191a8af04802aa4277c25634fdbe7be89064f
Artur <arturpisze@gmail.com>
2026-09-11 15:31:35 +0200

    0.1: log przyciskow MediaSession (test Again z ekranu blokady)

--- 01-handsfree/sw.js
+++ 01-handsfree/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'memovox-proba-01-v5';
+const CACHE = 'memovox-proba-01-v6';
 const FILES = ['./', './index.html', './manifest.webmanifest', './audio/q1.wav', './audio/a1.wav', './audio/q2.wav', './audio/a2.wav', './audio/q3.wav', './audio/a3.wav'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting())); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
